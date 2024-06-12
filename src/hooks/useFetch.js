ad65b530346c143db08f52659cd93805e3588ae0
danishkodemonkey <humberjini@hotmail.com>
2024-06-12 16:18:37 +0200

    breaking: custom hook for fetching all posts using API functions

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = (fetchFunction) => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const result = await fetchFunction();
+                setData(result);
+            } catch (error) {
+                console.error('Error fetching data: ', error);
+                setError('Failed to fetch data');
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, [fetchFunction]);
+    return { data, loading, error };
+};
+
+export default useFetch;
