8d36c055f907104d23dfb6979cc41bcb5e718f12
danishkodemonkey <humberjini@hotmail.com>
2024-06-17 18:12:40 +0200

    BREAKING: refactor of createpost component to dual use for edit operations

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,20 +1,50 @@
-import { useState, useContext } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+import { useState, useEffect, useContext, useRef } from 'react';
 import AuthContext from '../AuthContext';
-import { useNavigate } from 'react-router-dom';
-import { createPost } from '../api';
+import { useNavigate, useParams } from 'react-router-dom';
+import { createPost, fetchPostById, editPost } from '../api';
 import { Link } from 'react-router-dom';
+import { Editor } from '@tinymce/tinymce-react';
 
 const CreatePost = () => {
     const { user } = useContext(AuthContext);
     const navigate = useNavigate();
+    const { postId } = useParams();
+    const editorRef = useRef(null);
 
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [published, setPublished] = useState(false);
     const [error, setError] = useState(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-    const handleEditorChange = (content, editor) => {
+    useEffect(() => {
+        if (postId) {
+            console.log('PostId received, setting edit mode to true', postId);
+            setIsEditing(true);
+            fetchPost();
+        } else {
+            // no data to fetch (new post)
+            setLoading(false);
+        }
+    }, [postId]);
+
+    const fetchPost = async () => {
+        console.log('fetching post...');
+        try {
+            const fetchedPost = await fetchPostById(postId);
+            console.log('Post fetched', fetchedPost);
+            setTitle(fetchedPost.title);
+            setContent(fetchedPost.text);
+            setPublished(fetchedPost.published);
+            setLoading(false);
+        } catch (error) {
+            setError(error.message || 'Failed to fetch post');
+            setLoading(false);
+        }
+    };
+
+    const handleEditorChange = (content) => {
         setContent(content);
     };
 
@@ -28,8 +58,13 @@ const CreatePost = () => {
             author: user._id || 'Anonymous',
         };
         try {
-            const response = await createPost(post);
-            console.log('Post created successfully!', response);
+            if (isEditing) {
+                await editPost(postId, post);
+                alert('Post updated successfully!');
+            } else {
+                await createPost(post);
+                alert('Post created successfully!');
+            }
             navigate('/');
         } catch (error) {
             console.error('Error creating post:', error.message);
@@ -37,6 +72,9 @@ const CreatePost = () => {
         }
     };
 
+    if (loading) {
+        return <div>Loading...</div>;
+    }
     return (
         <div className='max-w-5xl mx-auto px-4 py-8 bg-slate-200 rounded-lg'>
             <Link
@@ -45,7 +83,9 @@ const CreatePost = () => {
             >
                 Return
             </Link>
-            <h1 className='text-3xl font-bold mb-4'>Create New Post</h1>
+            <h1 className='text-3xl font-bold mb-4'>
+                {isEditing ? 'Edit post' : 'Create New Post'}
+            </h1>
             {error && <p className='text-red-500'>{error}</p>}
             <form onSubmit={handleSubmit}>
                 <div className='mb-4'>
@@ -71,21 +111,22 @@ const CreatePost = () => {
                         Content
                     </label>
                     <Editor
+                        onInit={(evt, editor) => (editorRef.current = editor)}
                         apiKey={import.meta.env.VITE_TINYMCE_API_KEY}
-                        value={content}
                         init={{
                             height: 500,
-                            menubar: false,
+                            menubar: true,
                             plugins: [
-                                'advlist autolink lists link image charmap print preview anchor',
+                                'advlist autolink lists link image charmap preview anchor',
                                 'searchreplace visualblocks code fullscreen',
-                                'insertdatetime media table paste code help wordcount',
+                                'insertdatetime media table code help wordcount',
                             ],
                             toolbar:
-                                'undo redo | formatselect | bold italic backcolor | \
-                                alignleft aligncenter alignright alignjustify | \
-                                bullist numlist outdent indent | removeformat | help',
+                                'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+                            content_style:
+                                'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
                         }}
+                        value={content}
                         onEditorChange={handleEditorChange}
                     />
                 </div>
@@ -107,7 +148,7 @@ const CreatePost = () => {
                 </div>
                 <div>
                     <button className='inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-offset-2 focus:ring-teal-500'>
-                        Create post
+                        {isEditing ? 'Update Post' : 'Create post'}
                     </button>
                 </div>
             </form>
